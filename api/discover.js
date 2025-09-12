bd54147296ada029c2baa5ad8a5e5f6b3bf84392
osrlazlo <zxp1254@proton.me>
2025-09-11 23:00:42 -0400

    idk what to say anymore, discover api fixed (i think)

--- api/discover.js
+++ api/discover.js
@@ -20,14 +20,14 @@ async function handler(req, res) {
 
     try {
 
-        const url = `https://api.themoviedb.org/3/discover/
-                    movie?include_adult=false&include_video=false&language=en-US&page=1
-                    ${filterParams.dateFrom ? "&primary_release_date.gte="+filterParams.dateFrom+"-01-01" : ""}
-                    ${filterParams.dateTo ? "&primary_release_date.lte="+filterParams.dateTo+"-01-01" : ""}
-                    &sort_by=${filterParams.sortBy}
-                    &vote_average.gte=${filterParams.rate}
-                    ${filterParams.genre && filterParams.genres.length ? "&with_genres="+filterParams:""}
-                    ${filterParams.country ? "&with_origin_country="+filterParams.countries.id:""}`
+        const url = `https://api.themoviedb.org/3/discover/`+
+                    `movie?include_adult=false&include_video=false&language=en-US&page=1`+
+                    `${filterParams.dateFrom ? "&primary_release_date.gte="+filterParams.dateFrom+"-01-01" : ""}`+
+                    `${filterParams.dateTo ? "&primary_release_date.lte="+filterParams.dateTo+"-01-01" : ""}`+
+                    `&sort_by=${filterParams.sortBy}`+
+                    `&vote_average.gte=${filterParams.rate}`+
+                    `${filterParams.genre && filterParams.genres.length ? "&with_genres="+filterParams:""}`+
+                    `${filterParams.country ? "&with_origin_country="+filterParams.countries.id:""}`
         
         console.log("URL:"+url)
         
