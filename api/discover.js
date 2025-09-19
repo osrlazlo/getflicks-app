498548d9ca4f16d9dc1db4cdb77cbcc3290c281e
osrlazlo <zxp1254@proton.me>
2025-09-18 23:50:18 -0400

    fixed fecth url issue

--- api/discover.js
+++ api/discover.js
@@ -21,14 +21,14 @@ export default async function handler(req, res) {
     try {
 
         const url = `https://api.themoviedb.org/3/discover/`+
-                    `movie?include_adult=false&include_video=false&language=en-US`+
-                    `&page=${filterParams.page ? filterParams.page:1}`+
-                    `${filterParams.dateFrom ? "&primary_release_date.gte="+filterParams.dateFrom : ""}`+
-                    `${filterParams.dateTo ? "&primary_release_date.lte="+filterParams.dateTo : ""}`+
-                    `&sort_by=${filterParams.sortBy}`+
-                    `&vote_average.gte=${filterParams.rate}`+`&vote_count.gte=${filterParams.voteCount}`+
-                    `${filterParams.genres ? "&with_genres="+filterParams.genres:""}`+
-                    `${filterParams.country ? "&with_origin_country="+filterParams.country:""}`
+            `movie?include_adult=false&include_video=false&language=en-US`+
+            `&page=${filterParams.page ? filterParams.page:1}`+
+            `${filterParams.dateFrom ? "&primary_release_date.gte="+filterParams.dateFrom+"-01-01" : ""}`+
+            `${filterParams.dateTo ? "&primary_release_date.lte="+filterParams.dateTo+"-12-31" : ""}`+
+            `&sort_by=${filterParams.sortBy}`+
+            `&vote_average.gte=${filterParams.rate}`+`&vote_count.gte=${filterParams.voteCount}`+
+            `${filterParams.genres ? "&with_genres="+filterParams.genres:""}`+
+            `${filterParams.country ? "&with_origin_country="+filterParams.country:""}`
         
         console.log("URL:"+url)
         
